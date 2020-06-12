1c513eabc0d0ed37fba4681ca70c6c4a3e7cec35
ivano-vvv <ivanovlad2010@gmail.com>
2020-06-12 14:11:01 +0300

    Update store.js
    tagsreducer added

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers } from "redux";
 import articlesReducer from "./reducers/articleReducer";
 import previewCardReducer from "./reducers/previewCardReducer";
 import { reducer as formReducer } from 'redux-form';
+import tagsReducer from "./reducers/tagsReducer";
 
 let reducers = combineReducers({
   articlesPack: articlesReducer,
   previewCardData: previewCardReducer,
+  tags: tagsReducer,
   form: formReducer,
 });
 
