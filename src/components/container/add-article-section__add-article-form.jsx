6b9a4f7d3e7d935af82821a499e8d4bc8dd87344
ivano-vvv <ivanovlad2010@gmail.com>
2020-06-20 11:22:20 +0300

    Update add-article-section__add-article-form.jsx
    unnecessary import deleted

--- src/components/container/add-article-section__add-article-form.jsx
+++ src/components/container/add-article-section__add-article-form.jsx
@@ -3,7 +3,6 @@ import AddArticleForm from "../display/add-article-form";
 import { reduxForm, reset } from "redux-form";
 import { useDispatch, useSelector } from "react-redux";
 import { saveArticle } from "../../redux/reducers/articleReducer";
-import { createTags } from "../../redux/reducers/tagsReducer";
 
 export default function AddArticleFormContainer(props) {
   const dispatch = useDispatch();
