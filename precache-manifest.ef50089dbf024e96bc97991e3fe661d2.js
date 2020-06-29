62f0fcd77cbde776af0bae6512f92c697aee9ca0
ivano-vvv <ivanovlad2010@gmail.com>
2020-06-29 19:25:50 +0300

    Updates

---
+++ precache-manifest.ef50089dbf024e96bc97991e3fe661d2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3c0209f477a80d9211570ddfb78679",
+    "url": "/to-reader/index.html"
+  },
+  {
+    "revision": "15a9dac006ae605f1f53",
+    "url": "/to-reader/static/css/main.e2e4c357.chunk.css"
+  },
+  {
+    "revision": "13aeddfabc0f8eb0bf23",
+    "url": "/to-reader/static/js/2.ab8a3adb.chunk.js"
+  },
+  {
+    "revision": "0042ed35eedf85f73c564b78f2a06746",
+    "url": "/to-reader/static/js/2.ab8a3adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a9dac006ae605f1f53",
+    "url": "/to-reader/static/js/main.cb847de9.chunk.js"
+  },
+  {
+    "revision": "ba8013e9228dbb3811d8",
+    "url": "/to-reader/static/js/runtime-main.a9ac0551.js"
+  },
+  {
+    "revision": "bc61ffa1ef8108aebeb23b794e7405db",
+    "url": "/to-reader/static/media/closing_cross.bc61ffa1.svg"
+  },
+  {
+    "revision": "03d2ff5536959ab682d373c3a2e10d41",
+    "url": "/to-reader/static/media/cover-default.03d2ff55.svg"
+  },
+  {
+    "revision": "a1d63767a87bb9da7d5b0ce4945cdeea",
+    "url": "/to-reader/static/media/edit.a1d63767.svg"
+  },
+  {
+    "revision": "492cd0a98634419cc454e12f32510a1c",
+    "url": "/to-reader/static/media/firstList-false.492cd0a9.svg"
+  },
+  {
+    "revision": "9894da430fd38533042a60bb7dbb50f3",
+    "url": "/to-reader/static/media/firstList-true.9894da43.svg"
+  },
+  {
+    "revision": "735929f9b340bb72390898ac0289cd57",
+    "url": "/to-reader/static/media/isRead-false.735929f9.svg"
+  },
+  {
+    "revision": "bd7173a637d77de0eed0b3f19e1cdd80",
+    "url": "/to-reader/static/media/isRead-true.bd7173a6.svg"
+  }
+]);
